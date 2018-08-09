66b36279d809e4216f3b4b0156589217ed4b3b8e
shailendra.suriyal <shailendra.suriyal@intimetec.com>
2018-08-09 15:53:17 +0530

    Conditional rendering of list completed

--- src/containers/series/index.js
+++ src/containers/series/index.js
@@ -4,29 +4,50 @@ class Series extends Component {
 
 
   state = {
-    series: []
+    series: [],
+    seriesName: '',
+    isFetching: false
   }
 
   componentDidMount() {
-   
   }
 
   onSeriesInputChange = e => {
+      this.setState({seriesName: e.target.value, isFetching: true})
     fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
     .then((response) => response.json())
-    .then(json => this.setState({series: json}))
+    .then(json => this.setState({series: json, isFetching: false}))
       console.log(e)
       console.log(e.target.value)
   }
 
     render() {
+        const{ series, seriesName, isFetching } = this.state
         return(
-            <div>The length of the series array  = {this.state.series.length}
-                <div>
-                    <input type="text" onChange={this.onSeriesInputChange}/>
-                </div>
-            <SeriesList list= {this.state.series} />
+            <div>
                 
+                <div>
+                    <input 
+                    value = {seriesName}
+                    type="text" 
+                    onChange={this.onSeriesInputChange}/>
+                </div>
+                {
+                    series.length === 0 && seriesName.trim() === ''
+                    &&
+                    <p> Please Enter series name into the input </p>
+                }
+                {
+                    series.length === 0 && seriesName.trim() !== ''
+                    &&
+                    <p> No tv series has been found with this name </p>
+                } 
+                {
+                    isFetching && <p>Loading....</p>
+                }
+                {
+                    !isFetching && <SeriesList list= {this.state.series} />
+                }
             </div>
         )
     }
