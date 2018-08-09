562e344c5eb63d368197abdf3089ab3a19d7cdff
shailendra.suriyal <shailendra.suriyal@intimetec.com>
2018-08-09 13:00:38 +0530

    Added state variable and rearranged the code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import Intro from './components/Intro'
+import Intro from '../Intro'
 //functional component
 //custom component should be Capitalized
 //Lowercase are built in components
 
 
 class App extends Component {
+
+  state = {
+    series: []
+  }
   render() {
     return (
       <div className="App">
@@ -14,6 +18,7 @@ class App extends Component {
           <h1 className="App-title">Tv Series List</h1>
         </header>
         <Intro message="Here you can find all of your most loved series"/>
+        The length of the series array  = {this.state.series.length}
       </div>
     );
   }
