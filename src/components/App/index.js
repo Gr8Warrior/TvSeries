8fa02709db61e454ff1986e4a173223c0a7fe1a5
shailendra.suriyal <shailendra.suriyal@intimetec.com>
2018-08-09 15:15:41 +0530

    added components to do Separation Of Concerns

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,22 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import Intro from '../Intro'
 import 'whatwg-fetch'
+import Series from '../../containers/series'
 //functional component
 //custom component should be Capitalized
 //Lowercase are built in components
 
-
 class App extends Component {
 
-  state = {
-    series: []
-  }
-
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-    .then((response) => response.json())
-    .then(json => this.setState({series: json}))
-  }
 
   render() {
     return (
@@ -26,7 +17,7 @@ class App extends Component {
           <h1 className="App-title">Tv Series List</h1>
         </header>
         <Intro message="Here you can find all of your most loved series"/>
-        The length of the series array  = {this.state.series.length}
+        <Series/>
       </div>
     );
   }
